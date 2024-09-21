2b69f85141a00838966dc5671697516a2f9dffda
mosefatullah <mosefatullah@gmail.com>
2024-09-21 15:21:00 +0600

    .

--- unnamed/vite.config.ts
+++ unnamed/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+ base: "/NASA-Space-Apps-Challenge-2024/",
+ plugins: [react()],
+});

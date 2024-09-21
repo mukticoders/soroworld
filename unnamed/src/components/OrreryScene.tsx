6871b1fd8fd0a2c30de7fc981e1db09c1a1f8017
mosefatullah <mosefatullah@gmail.com>
2024-09-21 16:30:46 +0600

    .

--- unnamed/src/components/OrreryScene.tsx
+++ unnamed/src/components/OrreryScene.tsx
@@ -4,301 +4,177 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import CelestialBody from "./CelestialBody";
 import Orbit from "./Orbit";
 import Stars from "./Stars";
+import { celestialBodies } from "../../public/data/orrery";
 import { NEO, Planet } from "../types/data";
 
-const OrreryScene: React.FC<{
- setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
- setData: React.Dispatch<React.SetStateAction<Planet | NEO | null>>;
-}> = ({ setIsOpen, setData }) => {
- const MAX_ZOOM_OUT = 1000;
- const PLANET_RADIUS_SCALE = 25;
- const SOLAR_SYSTEM_INCLINATION = 30;
+class Orrery {
+  scene: THREE.Scene;
+  camera: THREE.PerspectiveCamera;
+  renderer: THREE.WebGLRenderer;
+  controls: OrbitControls;
+  solarSystemGroup: THREE.Group;
+  celestialAnimations: Array<(time: number) => void> = [];
+  raycaster: THREE.Raycaster;
+  mouse: THREE.Vector2;
+  mountRef: React.MutableRefObject<HTMLDivElement | null>;
 
- const celestialBodies = [
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDydjdQ1RL4q2aBOVkHfCgIMpEjb0IFBiiUg&s",
-   name: "Sun",
-   group: "Star",
-   params: {
-    size: 5,
-    orbitRadius: 0,
-    inclination: 0,
-    velocity: 0,
-   },
-  },
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQlXCG5Zeg_PJXAHKYmRSBgkjdehPuRoDb8Q&s",
-   name: "Earth",
-   group: "Planet",
-   params: {
-    size: 1.5,
-    orbitRadius: 1.0 * PLANET_RADIUS_SCALE,
-    inclination: 0.00005,
-    velocity: 29.8,
-   },
-  },
-  {
-   texture:
-    "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
-   name: "Mars",
-   group: "Planet",
-   params: {
-    size: 1,
-    orbitRadius: 1.524 * PLANET_RADIUS_SCALE,
-    inclination: 1.85,
-    velocity: 24.1,
-   },
-  },
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-OMXsZFqfhTgoP2i4r0KdOAsGMHxuHKD0PQ&s",
-   name: "Venus",
-   group: "Planet",
-   params: {
-    size: 1.2,
-    orbitRadius: 0.72 * PLANET_RADIUS_SCALE,
-    inclination: 3.39,
-    velocity: 35.0,
-   },
-  },
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTORdzdwfM7t3gPHwI267R9fiN8-Fqe2Dr8yg&s",
-   name: "Mercury",
-   group: "Planet",
-   params: {
-    size: 0.6,
-    orbitRadius: 0.39 * PLANET_RADIUS_SCALE,
-    inclination: 7.0,
-    velocity: 47.4,
-   },
-  },
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_ABVh6X-rxANutcMkEqX0Q6fQtFt7ERZPkQ&s",
-   name: "Jupiter",
-   group: "Planet",
-   params: {
-    size: 5.5,
-    orbitRadius: 5.2 * PLANET_RADIUS_SCALE,
-    inclination: 1.31,
-    velocity: 13.1,
-   },
-  },
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFEBAq2y8p5pY3Q3JR4DE-fAiyFrXeGTj6VA&s",
-   name: "Saturn",
-   group: "Planet",
-   params: {
-    size: 4.5,
-    orbitRadius: 9.58 * PLANET_RADIUS_SCALE,
-    inclination: 2.49,
-    velocity: 9.7,
-   },
-  },
-  {
-   texture:
-    "https://upload.wikimedia.org/wikipedia/commons/9/95/Solarsystemscope_texture_2k_uranus.jpg",
-   name: "Uranus",
-   group: "Planet",
-   params: {
-    size: 3.9,
-    orbitRadius: 19.18 * PLANET_RADIUS_SCALE,
-    inclination: 0.77,
-    velocity: 6.8,
-   },
-  },
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5m6I1cNvdxJo1hMYBzgmMzcD1viyiItRiyg&s",
-   name: "Neptune",
-   group: "Planet",
-   params: {
-    size: 3.8,
-    orbitRadius: 30.07 * PLANET_RADIUS_SCALE,
-    inclination: 1.77,
-    velocity: 5.4,
-   },
-  },
-  // Adding NEC, PHA, and NEA
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy78KEXOeYxXtHY6NVxVmtCKXDMiJd1__YRA&s",
-   name: "NEC - Comet Holmes",
-   group: "NEO",
-   params: {
-    size: 0.2,
-    orbitRadius: 1.5 * PLANET_RADIUS_SCALE,
-    inclination: 10.0,
-    velocity: 10,
-   },
-  },
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy78KEXOeYxXtHY6NVxVmtCKXDMiJd1__YRA&s",
-   name: "PHA - Asteroid 46610",
-   group: "NEO",
-   params: {
-    size: 0.2,
-    orbitRadius: 1.2 * PLANET_RADIUS_SCALE,
-    inclination: 8.0,
-    velocity: 10,
-   },
-  },
-  {
-   texture:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy78KEXOeYxXtHY6NVxVmtCKXDMiJd1__YRA&s",
-   name: "NEA - Asteroid 2004 EW95",
-   group: "NEO",
-   params: {
-    size: 0.2,
-    orbitRadius: 1.0 * PLANET_RADIUS_SCALE,
-    inclination: 12.0,
-    velocity: 10,
-   },
-  },
- ];
+  constructor(
+    mountRef: React.MutableRefObject<HTMLDivElement | null>,
+    private setData: React.Dispatch<React.SetStateAction<Planet | NEO | null>>,
+    private setIsOpen: React.Dispatch<React.SetStateAction<boolean>>,
+    private MAX_ZOOM_OUT: number = 1000,
+    private SOLAR_SYSTEM_INCLINATION: number = 30
+  ) {
+    this.mountRef = mountRef;
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x000000);
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    this.camera.position.z = 30;
+    this.renderer = new THREE.WebGLRenderer();
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.solarSystemGroup = new THREE.Group();
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector2();
+    this.initControls();
+    this.initScene();
+  }
 
- const mountRef = useRef<HTMLDivElement>(null);
+  initControls() {
+    this.controls.enableZoom = true;
+    this.controls.enablePan = true;
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.05;
+    this.controls.screenSpacePanning = true;
+    this.controls.maxDistance = this.MAX_ZOOM_OUT;
+  }
 
- useEffect(() => {
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x000000);
+  initScene() {
+    this.scene.add(this.solarSystemGroup);
+    const textureLoader = new THREE.TextureLoader();
 
-  const camera = new THREE.PerspectiveCamera(
-   75,
-   window.innerWidth / window.innerHeight,
-   0.1,
-   1000
-  );
-  camera.position.z = 30;
+    celestialBodies.forEach((body) => {
+      const { texture, glb, name, params } = body;
+      const { size, orbitRadius, inclination, velocity } = params;
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  mountRef.current?.appendChild(renderer.domElement);
+      const _texture = texture ? textureLoader.load(texture) : null;
+      if (_texture) {
+        _texture.minFilter = THREE.LinearFilter;
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableZoom = true;
-  controls.enablePan = true;
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.05;
-  controls.screenSpacePanning = true;
-  controls.maxDistance = MAX_ZOOM_OUT;
+        const celestialBody = new CelestialBody({
+          texture: _texture,
+          size,
+          orbitRadius,
+          name,
+        });
 
-  // Create a solar system group
-  const solarSystemGroup = new THREE.Group();
-  scene.add(solarSystemGroup);
+        const bodyGroup = new THREE.Group();
+        bodyGroup.rotation.x = THREE.MathUtils.degToRad(inclination);
+        bodyGroup.add(celestialBody.mesh);
+        if (celestialBody.sprite) {
+          bodyGroup.add(celestialBody.sprite);
+        }
+        this.solarSystemGroup.add(bodyGroup);
 
-  // Loaders
-  const textureLoader = new THREE.TextureLoader();
+        if (orbitRadius > 0) {
+          const bodyOrbit = Orbit(orbitRadius, body.group);
+          bodyGroup.add(bodyOrbit.line);
+        }
 
-  // Hold references to celestial bodies for animation
-  const celestialAnimations: Array<(time: number) => void> = [];
-  celestialBodies.forEach((body) => {
-   const { texture, name, params } = body;
-   const { size, orbitRadius, inclination, velocity } = params;
+        const animate = (time: number) => {
+          const angle = time * velocity * 0.01;
+          celestialBody.updatePosition(angle);
+        };
 
-   if (texture) {
-    const _texture = textureLoader.load(texture);
-    _texture.minFilter = THREE.LinearFilter;
-
-    const celestialBody = new CelestialBody({
-     texture: _texture,
-     size,
-     orbitRadius,
-     name,
+        this.celestialAnimations.push(animate);
+      }
     });
 
-    const bodyGroup = new THREE.Group();
-    bodyGroup.rotation.x = THREE.MathUtils.degToRad(inclination);
-    bodyGroup.add(celestialBody.mesh);
-    if (celestialBody.sprite) {
-     bodyGroup.add(celestialBody.sprite);
-    }
-    solarSystemGroup.add(bodyGroup);
+    const stars = Stars();
+    this.scene.add(stars.mesh);
 
-    if (orbitRadius > 0) {
-     const bodyOrbit = Orbit(orbitRadius, body.group);
-     bodyGroup.add(bodyOrbit.line);
-    }
+    const solarSystemInclinationRad = THREE.MathUtils.degToRad(
+      this.SOLAR_SYSTEM_INCLINATION
+    );
+    this.solarSystemGroup.rotation.x = solarSystemInclinationRad;
+  }
 
-    const animate = (time: number) => {
-     const angle = time * velocity * 0.01;
-     celestialBody.updatePosition(angle);
+  handleResize = () => {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  };
+
+  handleClick = (event: MouseEvent) => {
+    event.preventDefault();
+
+    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    const intersects = this.raycaster.intersectObjects(this.scene.children, true);
+
+    intersects.forEach((intersect) => {
+      if (intersect.object instanceof THREE.Mesh) {
+        const data = celestialBodies.find(
+          (body) => body.name === intersect.object.name
+        );
+        if (data) {
+          this.setData(data);
+          this.setIsOpen(true);
+        }
+      }
+    });
+  };
+
+  renderScene() {
+    const time = Date.now() * 0.0001;
+    this.celestialAnimations.forEach((animate) => animate(time));
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  start() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.mountRef.current?.appendChild(this.renderer.domElement);
+    window.addEventListener("resize", this.handleResize);
+    window.addEventListener("click", this.handleClick);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      this.renderScene();
     };
 
-    celestialAnimations.push(animate);
-   }
-  });
+    animate();
+  }
 
-  const stars = Stars();
-  scene.add(stars.mesh);
+  cleanup() {
+    this.mountRef.current?.removeChild(this.renderer.domElement);
+    window.removeEventListener("resize", this.handleResize);
+    window.removeEventListener("click", this.handleClick);
+  }
+}
 
-  const solarSystemInclinationRad = THREE.MathUtils.degToRad(
-   SOLAR_SYSTEM_INCLINATION
-  );
-  solarSystemGroup.rotation.x = solarSystemInclinationRad;
+const OrreryScene: React.FC<{
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setData: React.Dispatch<React.SetStateAction<Planet | NEO | null>>;
+}> = ({ setIsOpen, setData }) => {
+  const mountRef = useRef<HTMLDivElement>(null);
 
-  // Raycaster for click detection
-  const raycaster = new THREE.Raycaster();
-  const mouse = new THREE.Vector2();
+  useEffect(() => {
+    const orrery = new Orrery(mountRef, setData, setIsOpen);
+    orrery.start();
 
-  const handleMouseClick = (event: MouseEvent) => {
-   event.preventDefault();
+    return () => {
+      orrery.cleanup();
+    };
+  }, []);
 
-   // Calculate mouse position in normalized device coordinates (-1 to +1) for both components
-   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-   // Update the raycaster
-   raycaster.setFromCamera(mouse, camera);
-
-   // Find intersections
-   const intersects = raycaster.intersectObjects(scene.children, true);
-
-   intersects.forEach((intersect) => {
-    if (intersect.object instanceof THREE.Mesh) {
-     const data = celestialBodies.find(
-      (body) => body.name === intersect.object.name
-     );
-     if (data) {
-      setData(data);
-      setIsOpen(true);
-     }
-    }
-   });
-  };
-
-  window.addEventListener("click", handleMouseClick);
-
-  const renderLoop = () => {
-   requestAnimationFrame(renderLoop);
-
-   const time = Date.now() * 0.0001; // Time in seconds
-   celestialAnimations.forEach((animate) => animate(time));
-
-   controls.update();
-   renderer.render(scene, camera);
-  };
-
-  renderLoop();
-
-  window.addEventListener("resize", () => {
-   camera.aspect = window.innerWidth / window.innerHeight;
-   camera.updateProjectionMatrix();
-   renderer.setSize(window.innerWidth, window.innerHeight);
-  });
-
-  return () => {
-   mountRef.current?.removeChild(renderer.domElement);
-   window.removeEventListener("click", handleMouseClick);
-  };
- }, []);
-
- return <div ref={mountRef} />;
+  return <div ref={mountRef} />;
 };
 
 export default OrreryScene;

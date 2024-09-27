a10dbe0786e002adf76d6a53794c8f1245f6aecf
mosefatullah <mosefatullah@gmail.com>
2024-09-28 01:56:10 +0600

    .

--- unnamed/src/components/home/OrreryScene.tsx
+++ unnamed/src/components/home/OrreryScene.tsx
@@ -7,110 +7,50 @@ import Stars from "./Stars";
 import { celestialBodies } from "../../../public/data/orrery";
 import fetchNeoData from "./../../../public/data/fetchNEO";
 import { Celestial } from "../../types/data";
+import { useNavigate } from "react-router-dom";
 
-/* https://images-api.nasa.gov/search?q=earth */
+const OrreryScene = ({ setIsOpen, setData }) => {
+ const mountRef = useRef(null);
+ const navigate = useNavigate();
 
-class Orrery {
- scene: THREE.Scene;
- camera: THREE.PerspectiveCamera;
- renderer: THREE.WebGLRenderer;
- controls: OrbitControls;
- solarSystemGroup: THREE.Group;
- celestialAnimations: Array<(time: number) => void> = [];
- raycaster: THREE.Raycaster;
- mouse: THREE.Vector2;
- mountRef: React.MutableRefObject<HTMLDivElement | null>;
+ useEffect(() => {
+  // Initialize Scene, Camera, Renderer, and Controls
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x000000);
 
- constructor(
-  mountRef: React.MutableRefObject<HTMLDivElement | null>,
-  private setData: React.Dispatch<React.SetStateAction<Celestial | null>>,
-  private setIsOpen: React.Dispatch<React.SetStateAction<boolean>>,
-  private MAX_ZOOM_OUT: number = 200,
-  private SOLAR_SYSTEM_INCLINATION: number = 30
- ) {
-  this.mountRef = mountRef;
-  this.scene = new THREE.Scene();
-  this.scene.background = new THREE.Color(0x000000);
-  this.camera = new THREE.PerspectiveCamera(
+  const camera = new THREE.PerspectiveCamera(
    75,
    window.innerWidth / window.innerHeight,
    0.1,
    1000
   );
-  this.camera.position.z = 30;
-  this.renderer = new THREE.WebGLRenderer();
-  this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-  this.solarSystemGroup = new THREE.Group();
-  this.raycaster = new THREE.Raycaster();
-  this.mouse = new THREE.Vector2();
-  this.initControls();
-  this.initScene();
- }
+  camera.position.z = 30;
 
- initControls() {
-  this.controls.enableZoom = true;
-  this.controls.enablePan = true;
-  this.controls.enableDamping = true;
-  this.controls.dampingFactor = 0.05;
-  this.controls.screenSpacePanning = true;
-  this.controls.maxDistance = this.MAX_ZOOM_OUT;
- }
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  mountRef?.current?.appendChild(renderer.domElement);
 
- initScene() {
-  this.scene.add(this.solarSystemGroup);
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableZoom = true;
+  controls.enablePan = true;
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+  controls.screenSpacePanning = true;
+  controls.maxDistance = 200;
+
+  const solarSystemGroup = new THREE.Group();
+  scene.add(solarSystemGroup);
+
+  const raycaster = new THREE.Raycaster();
+  const mouse = new THREE.Vector2();
+
+  // Initialize celestial animations
+  const celestialAnimations: ((time: number) => void)[] = [];
   const textureLoader = new THREE.TextureLoader();
 
-  fetchNeoData(
-   (
-    celestialBodies: {
-     texture: string;
-     name: string;
-     group: string;
-     params: {
-      size: number;
-      orbitRadius: number;
-      inclination: number;
-      velocity: number;
-     };
-    }[]
-   ) => {
-    celestialBodies.forEach((body) => {
-     const { texture, name, params, group } = body;
-     const { size, orbitRadius, inclination, velocity } = params;
-
-     const _texture = texture ? new THREE.TextureLoader().load(texture) : null;
-     if (_texture) {
-      _texture.minFilter = THREE.LinearFilter;
-
-      const celestialBody = new CelestialBody({
-       texture: _texture,
-       size,
-       orbitRadius,
-       name,
-       group,
-      });
-
-      const bodyGroup = new THREE.Group();
-      bodyGroup.rotation.x = THREE.MathUtils.degToRad(inclination);
-      bodyGroup.add(celestialBody.mesh);
-      if (celestialBody.sprite) {
-       bodyGroup.add(celestialBody.sprite);
-      }
-      this.solarSystemGroup.add(bodyGroup);
-
-      if (orbitRadius > 0) {
-       const bodyOrbit = Orbit(orbitRadius, body.group);
-       if (bodyOrbit.line) bodyGroup.add(bodyOrbit.line);
-      }
-
-      celestialBody.updatePosition(Math.random() * 360);
-     }
-    });
-   }
-  );
-
-  celestialBodies.forEach((body) => {
-   const { texture, glb, name, params, group } = body;
+  // Function to add celestial bodies to the scene
+  const addCelestialBody = (body: Celestial) => {
+   const { texture, name, params, group } = body;
    const { size, orbitRadius, inclination, velocity } = params;
 
    const _texture = texture ? textureLoader.load(texture) : null;
@@ -131,126 +71,111 @@ class Orrery {
     if (celestialBody.sprite) {
      bodyGroup.add(celestialBody.sprite);
     }
-    this.solarSystemGroup.add(bodyGroup);
+    solarSystemGroup.add(bodyGroup);
 
     if (orbitRadius > 0) {
-     const bodyOrbit = Orbit(orbitRadius, body.group);
+     const bodyOrbit = Orbit(orbitRadius, group);
      bodyGroup.add(bodyOrbit.line!);
     }
 
+    // Differentiate NEO movement logic
+    const isNEO = group === "NEC" || group === "NEA" || group === "PHA";
     const animate = (time: number) => {
-     const angle = time * velocity * 0.01;
+     const angle = isNEO ? Math.random() * 360 : time * velocity * 0.01; // Faster for NEOs or other logic
      celestialBody.updatePosition(angle);
     };
 
-    this.celestialAnimations.push(animate);
+    if (!isNEO) celestialAnimations.push(animate);
+    else animate(0);
    }
+  };
+
+  // Add Celestial Bodies from static data
+  celestialBodies.forEach(addCelestialBody);
+
+  // Fetch and Add NEOs
+  fetchNeoData((neoData) => {
+   neoData.forEach(addCelestialBody);
   });
 
+  // Add Stars to Scene
   const stars = Stars();
-  this.scene.add(stars.mesh);
+  scene.add(stars.mesh);
 
-  const solarSystemInclinationRad = THREE.MathUtils.degToRad(
-   this.SOLAR_SYSTEM_INCLINATION
-  );
-  this.solarSystemGroup.rotation.x = solarSystemInclinationRad;
- }
+  // Set Solar System Inclination
+  const solarSystemInclinationRad = THREE.MathUtils.degToRad(30);
+  solarSystemGroup.rotation.x = solarSystemInclinationRad;
 
- handleResize = () => {
-  this.camera.aspect = window.innerWidth / window.innerHeight;
-  this.camera.updateProjectionMatrix();
-  this.renderer.setSize(window.innerWidth, window.innerHeight);
- };
+  // Handle Window Resize
+  const handleResize = () => {
+   camera.aspect = window.innerWidth / window.innerHeight;
+   camera.updateProjectionMatrix();
+   renderer.setSize(window.innerWidth, window.innerHeight);
+  };
 
- handleClick = (event: MouseEvent) => {
-  event.preventDefault();
+  // Handle Mouse Click
+  const handleClick = (event: MouseEvent) => {
+   event.preventDefault();
 
-  this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-  this.raycaster.setFromCamera(this.mouse, this.camera);
-  const intersects = this.raycaster.intersectObjects(this.scene.children, true);
+   raycaster.setFromCamera(mouse, camera);
+   const intersects = raycaster.intersectObjects(scene.children, true);
 
-  intersects.forEach((intersect) => {
-   if (intersect.object instanceof THREE.Mesh) {
-    const data = celestialBodies.find(
-     (body) => body.name === intersect.object.name
-    );
-    if (data) {
-     this.setData(data);
-     this.setIsOpen(true);
-    } else {
-     fetchNeoData((c: Celestial[] | undefined) => {
-      const data1 = c?.find((body) => body.name === intersect.object.name);
-      if (data1) {
-       this.setData(data1!);
-       this.setIsOpen(true);
-      }
-     });
+   const goToPage = (name: string, group: string) => {
+    navigate(`/celestial/${group}/${name}`);
+   };
+
+   intersects.forEach((intersect) => {
+    if (
+     intersect.object instanceof THREE.Mesh ||
+     intersect.object instanceof THREE.Sprite
+    ) {
+     const data = celestialBodies.find(
+      (body) => body.name === intersect.object.name
+     );
+     if (data) {
+      setData(data);
+      setIsOpen(true);
+      goToPage(data.name, data.group);
+     } else {
+      fetchNeoData((c) => {
+       const data1 = c?.find((body) => body.name === intersect.object.name);
+       if (data1) {
+        setData(data1);
+        setIsOpen(true);
+        goToPage(data1.name, data1.group);
+       }
+      });
+     }
     }
-   } else if (intersect.object instanceof THREE.Sprite) {
-    const data = celestialBodies.find(
-     (body) => body.name === intersect.object.name
-    );
-    if (data) {
-     this.setData(data);
-     this.setIsOpen(true);
-    } else {
-     fetchNeoData((c: Celestial[] | undefined) => {
-      const data1 = c?.find((body) => body.name === intersect.object.name);
+   });
+  };
 
-      if (data1) {
-       this.setData(data1!);
-       this.setIsOpen(true);
-      }
-     });
-    }
-   }
-  });
- };
-
- renderScene() {
-  const time = Date.now() * 0.0001;
-  this.celestialAnimations.forEach((animate) => animate(time));
-  this.controls.update();
-  this.renderer.render(this.scene, this.camera);
- }
-
- start() {
-  this.renderer.setSize(window.innerWidth, window.innerHeight);
-  this.mountRef.current?.appendChild(this.renderer.domElement);
-  window.addEventListener("resize", this.handleResize);
-  window.addEventListener("click", this.handleClick);
-
+  // Animation Loop
   const animate = () => {
    requestAnimationFrame(animate);
-   this.renderScene();
+   const time = Date.now() * 0.0001;
+   celestialAnimations.forEach((animate) => animate(time));
+   controls.update();
+   renderer.render(scene, camera);
   };
 
+  // Event Listeners
+  window.addEventListener("resize", handleResize);
+  window.addEventListener("click", handleClick);
+
+  // Start Animation
   animate();
- }
 
- cleanup() {
-  this.mountRef.current?.removeChild(this.renderer.domElement);
-  window.removeEventListener("resize", this.handleResize);
-  window.removeEventListener("click", this.handleClick);
- }
-}
-
-const OrreryScene: React.FC<{
- setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
- setData: React.Dispatch<React.SetStateAction<Celestial | null>>;
-}> = ({ setIsOpen, setData }) => {
- const mountRef = useRef<HTMLDivElement>(null);
-
- useEffect(() => {
-  const orrery = new Orrery(mountRef, setData, setIsOpen);
-  orrery.start();
-
+  // Cleanup on Unmount
   return () => {
-   orrery.cleanup();
+   mountRef.current.removeChild(renderer.domElement);
+   window.removeEventListener("resize", handleResize);
+   window.removeEventListener("click", handleClick);
   };
- }, []);
+ }, [mountRef, setData, setIsOpen, navigate]);
 
  return <div ref={mountRef} />;
 };
